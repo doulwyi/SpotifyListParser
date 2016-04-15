7471f3de7f111b011b8a11ef20c9726c9792fdd2
doulwyi <kanazawabruno@gmail.com>
2016-04-15 14:17:30 -0300

    Improvements on code

--- spotify.py
+++ spotify.py
@@ -1,28 +1,17 @@
-import json
 import re
 import sys
 import subprocess
+import urllib.parse
 import spotipy
 import urllib.parse
 import urllib.request
 from os.path import expanduser
+from bs4 import BeautifulSoup
 from spotipy.oauth2 import SpotifyClientCredentials
-from spotipy import oauth2 as spotify_web_oauth
-import spotipy.util as util
+
 
 client_id ='239f993e90b54895a1faebc8b6b2f485'
 client_secret ='e70808aa36e540c6b0dc545fbe4e5768'
-# redirect_uri ='http://listparser.com/callback'
-username = 'd.flucas'
-# username = '12144157524'
-list_ = '4Kls4WcczUw0Fj5XAx4Jbp'
-
-
-# list_ = '1UmF20zPbew2OHOiZfWwUA'
-
-# uri = 'spotify:user:spotify:playlist:5ILSWr90l2Bgk89xuhsysy'
-# spotify:user:d.flucas:playlist:4Kls4WcczUw0Fj5XAx4Jbp
-
 
 
 def runCMD(cmd, timeout=1):
@@ -44,13 +33,13 @@ def show_youtube_url(track, playlist, artist):
     full_search = track + ' ' + artist + ' audio'
     print(full_search)
     query_string = urllib.parse.urlencode({"search_query": full_search})
-    print(query_string)
+    # print(query_string)
     html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
-    print(html_content)
+    # print(html_content)
     search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
-    print(search_results)
+    # print(search_results)
     url = "http://www.youtube.com/watch?v=" + search_results[0]
-    print(url)
+    # print(url)
     download_audio_from_youtube(url, track, playlist, artist)
     print("http://www.youtube.com/watch?v=" + search_results[0])
 
@@ -59,11 +48,12 @@ def download_audio_from_youtube(url, track, playlist, artist):
     home = expanduser('~\Desktop')
     path = home + '\\' + playlist.replace(' ', '_')
     print(path)
-    artist_ = str(artist).replace(' ', '_').replace('/', '_')
-    track_ = str(track).replace(' ', '_').replace('/', '_')
-    print(artist_, track_)
-    command = "youtube-dl --newline --no-post-overwrites --no-playlist -x --audio-format mp3 --audio-quality 0 -o "
-    cmd = command + path + "\\" + track_ + "_" + artist_ + ".%(ext)s " + url
+    artist_ = str(artist).replace('/', '-')
+    track_ = str(track).replace('/', '-')
+    print("Downloading: " + artist_, track_)
+    command = 'youtube-dl --newline --no-post-overwrites --no-playlist -x --audio-format mp3 --audio-quality 0 -o "'
+    cmd = command + path + '\\' + track_ + '-' + artist_ + '.%(ext)s" ' + url
+    print(cmd)
 
     runCMD(cmd, 3)
 
@@ -73,30 +63,26 @@ if __name__ == '__main__':
     #     uri = sys.argv[1]
     # else:
     #     print ("Whoops, need your username!")
-    #     print ("usage: python user_playlists.py [URI]")
+    #     print ("usage: python spotify.py [URI]")
     #     sys.exit()
     # print ("Parsing: " + uri)
-    #
-    # playlist_re = re.compile("spotify:user:[\w]+:playlist:[\w]+")
-    # for playlist_uri in playlist_re.findall(uri):
-    #     segments = playlist_uri.split(":")
-    #     user_id = segments[2]
-    #     playlist_id = segments[4]
-    #
-    #     playlist = sp.user_playlist(user_id, playlist_id)
-    #
-    #     scope = " ".join([
-    #         "playlist-read-private",
-    #         "playlist-read-collaborative",
-    #     ])
-    # with open("client_secret_key.json") as creditials:
-    #     pass
+
+    uri = 'spotify:user:12144157524:playlist:4SUYNZGVFL7LxOUtBkjb7j'
+
+    playlist_re = re.compile("spotify:user:[\w]+:playlist:[\w]+")
+    for playlist_uri in playlist_re.findall(uri):
+        segments = playlist_uri.split(":")
+        print(segments)
+        user_id = segments[2]
+        print(user_id)
+        playlist_id = segments[4]
+        print(playlist_id)
+
 
     token = SpotifyClientCredentials(client_id, client_secret)
-    print(token)
     if token:
         sp = spotipy.Spotify(client_credentials_manager=token)
-        playlist = sp.user_playlist(username,list_,fields="tracks, next, name")
+        playlist = sp.user_playlist(user_id, playlist_id, fields="tracks, next, name")
         tracks = playlist['tracks']
         name_playlist = playlist['name']
         print('Playlist: ' + name_playlist)
@@ -105,4 +91,4 @@ if __name__ == '__main__':
             tracks = sp.next(tracks)
             show_tracks(tracks, name_playlist)
     else:
-        print ("Can't get token for", username)
+        print("Can't get token for", user_id)
